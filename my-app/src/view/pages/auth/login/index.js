84f70a7a677c7d2c478692f485dfe9112f5f54b9
Mariam0422 <mariam79.yan@gmail.com>
2024-07-28 01:29:36 +0400

    updrate register form

--- my-app/src/view/pages/auth/login/index.js
+++ my-app/src/view/pages/auth/login/index.js
@@ -1,9 +1,23 @@
-
+import { Form, Input, Button, Typography } from "antd";
+import './index.css'
 const Login = () => {
     return(
-        <div>
-           Login 
-        </div>
+        <div className="loginForm">       
+       <Typography.Title level={2}>
+        Login
+       </Typography.Title>
+       <Form layout="vertical">
+        <Form.Item label="Login" name="login">
+         <Input type="text" />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+         <Input type="text" />
+        </Form.Item>  
+        <Form.Item>
+            <Button>Log In</Button>
+        </Form.Item>
+       </Form>
+       </div>
     )
 }
 export default Login;
--- my-app/src/view/pages/auth/register/index.js
+++ my-app/src/view/pages/auth/register/index.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import { Typography, Input, Divider, Button, Form, notification, Select } from 'antd';
+import { Typography, Input, Divider, Button, Form, notification, Select, Radio, DatePicker,  } from 'antd';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth, getDoc, setDoc, doc, db } from '../../../../services/firebase/firebase';
+import { auth, setDoc, doc, db } from '../../../../services/firebase/firebase';
 import RegisterCover from '../../../../core/images/register.png'
 import AuthWrapper from '../../../components/shared/AuthWrapper';
+import './index.css'
 
 const {Title} = Typography;
 const option = [
@@ -18,10 +19,12 @@ const option = [
 ]
 const Register = () => {
   const [loading, setLoading] = useState(false);
+  const [dateOfBirth, setDateOfBirth] = useState("");
   const [formValues, setformValues] = useState({
     userValue: "",
     firstName: "",
     lastName: "",
+    gender: "",
     headLine: "",
     email: "",
     password: ""
@@ -30,15 +33,19 @@ const Register = () => {
   const handleChangeInput = (e, allvalues) => {
    setformValues(allvalues);
   }
+  const handleDateChange = (date, dateString) => {
+    setDateOfBirth(dateString);
+  };
+
   const handleRegister = async () => {
-    const { userValue, email, password, firstName, lastName, headLine} = formValues;  
+    const { userValue, email, password, firstName, lastName, headLine, gender} = formValues;  
     setLoading(true);
     try{
     const response = await createUserWithEmailAndPassword(auth, email, password);   
      const uid = response.user.uid;
      const createDoc = doc(db, "registerUsers", uid);
      setDoc(createDoc, {
-      userValue, firstName, lastName, headLine
+      userValue, firstName, lastName, gender, headLine, dateOfBirth
      })
     notification.success({
         message: "Success Register",
@@ -55,16 +62,17 @@ const Register = () => {
       setLoading(false);
      }
   }
-console.log(formValues)
+
     return ( 
-        <AuthWrapper coverImg={RegisterCover}>      
+        <AuthWrapper coverImg={RegisterCover} >      
           <Title level={2}>
           Register
          </Title>         
-         <Form layout='vertical' onValuesChange={handleChangeInput}>
+         <Form layout='vertical' onValuesChange={handleChangeInput} >
+          <div className='registerForm'>
+          <div>        
           <Form.Item label="Register for" name="userValue">
-            <Select options={option} placeholder="Student"/>        
-            
+            <Select options={option} placeholder="Student"/>      
           </Form.Item>
          <Form.Item name='firstName' label="First Name">
           <Input type='text'  placeholder='first name'  value={formValues.firstName}/>
@@ -73,18 +81,34 @@ console.log(formValues)
           <Input type='text' placeholder='last name' value={formValues.lastName}
            />
          </Form.Item>
+         <Form.Item label="Gender" name="gender">
+          <Radio.Group>
+            <Radio value="male"> Male </Radio>
+            <Radio value="female"> Female </Radio>
+          </Radio.Group>
+          </Form.Item>       
+         </div>
+
+
+
+         <div>
+          <Form.Item label="Select your date of birth">
+         <DatePicker onChange={handleDateChange} style={{width: "200px"}}/>
+          </Form.Item>
          <Form.Item name='headLine' label="Headline">
           <Input type='text'  placeholder='headLine' value={formValues.headLine}
           />
          </Form.Item>
-         <Form.Item name='email' label="Email">
+             <Form.Item name='email' label="Email">
           <Input type='email'  placeholder='email'   value={formValues.email}
            />
          </Form.Item>
          <Form.Item name='password' label="Password">
           <Input.Password  placeholder='password'   value={formValues.password}
             />
-         </Form.Item>
+         </Form.Item>       
+         </div>
+         </div>
          <Divider/>
          <Button className='button' onClick={handleRegister} loading={loading}>Register</Button>
          </Form>
